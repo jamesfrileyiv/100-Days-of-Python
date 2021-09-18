2be6ac03a2e8e89142288dc8cde245c7364fd8e7
jamesfrileyiv <jamesfrileyiv@gmail.com>
2021-09-17 23:31:56 -0400

    Dev (#4)
    * Classic Pong Game. Two players
    
    * Created a frogger type game using turtle library in python
    
    * Create README.md (#2)
    
    * Added functionality for tracking high score in snake game
    
    * removed unnecessary commented out code

--- Day 020-21 - Snake Game/scoreboard.py
+++ Day 020-21 - Snake Game/scoreboard.py
@@ -1,9 +1,13 @@
 from turtle import Turtle
+from os import path
 
 ALIGNMENT = "center"
 FONT = "Arial"
 FONT_SIZE = 24
 FONT_TYPE = "normal"
+HIGH_SCORE_FILE_PATH = "high_score.txt"
+DEFAULT_HIGH_SCORE = 0
+
 
 class ScoreBoard(Turtle):
 
@@ -13,8 +17,26 @@ class ScoreBoard(Turtle):
         self.hideturtle()
         self.penup()
         self.goto(x=0, y=260)
+        self.high_score = self.initialize_high_score()
         self.score = 0
         self.refresh_scoreboard()
+
+    @staticmethod
+    def initialize_high_score():
+        high_score = DEFAULT_HIGH_SCORE
+        if path.exists(HIGH_SCORE_FILE_PATH):
+            with open(file=HIGH_SCORE_FILE_PATH, mode="r") as f:
+                line = f.readline()
+            if line.isdecimal():
+                if int(line) > 0:
+                    high_score = int(line)
+            else:
+                with open(file=HIGH_SCORE_FILE_PATH, mode="w") as f:
+                    f.write(str(high_score))
+        else:
+            with open(file=HIGH_SCORE_FILE_PATH, mode="w") as f:
+                f.write(str(DEFAULT_HIGH_SCORE))
+        return high_score
 
     def increment_score(self):
         self.score += 1
@@ -22,7 +44,14 @@ class ScoreBoard(Turtle):
     def game_over(self):
         self.goto(0, 0)
         self.write(arg="Game Over", align=ALIGNMENT, font=(FONT, FONT_SIZE, FONT_TYPE))
+        if self.score > self.high_score:
+            self.write_high_score()
 
     def refresh_scoreboard(self):
         self.clear()
-        self.write(arg=f"Score: {self.score}", align=ALIGNMENT, font=(FONT, FONT_SIZE, FONT_TYPE))
+        self.write(arg=f"Score: {self.score}\tHigh Score: {self.high_score}",
+                   align=ALIGNMENT, font=(FONT, FONT_SIZE, FONT_TYPE))
+
+    def write_high_score(self):
+        with open(file=HIGH_SCORE_FILE_PATH, mode="w") as f:
+            f.write(str(self.score))
